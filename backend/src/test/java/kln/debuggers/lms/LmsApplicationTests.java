93383441a6735b82d7f0acf38a27fde71a5f1425
imasshaWe <imasha98.we@gmail.com>
2022-04-08 11:00:52 +0530

    initial commit

---
+++ backend/src/test/java/kln/debuggers/lms/LmsApplicationTests.java
@@ -0,0 +1,13 @@
+package kln.debuggers.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
