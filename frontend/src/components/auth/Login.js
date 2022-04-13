d6a3c29e2d2dac184ab52f21e6044f7b9bc85320
imasshaWe <imasha98.we@gmail.com>
2022-04-13 20:47:04 +0530

    add theme

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -7,7 +7,6 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import {createTheme, ThemeProvider} from '@mui/material/styles';
 import {useState} from "react";
 import {Alert} from "@mui/material";
 import {Link, useNavigate, useParams} from "react-router-dom";
@@ -17,8 +16,6 @@ import {useSignIn} from 'react-auth-kit'
 import {parseApiUrl, parseMessage} from "../../helpers/functions";
 import jwt_decode from "jwt-decode";
 import CopyrightView from "../common/CopyrightView";
-
-const theme = createTheme();
 
 export default function Login() {
     const [error, setError] = useState();
@@ -51,70 +48,69 @@ export default function Login() {
     }
 
     return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline/>
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
-                        <LockOutlinedIcon/>
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Log in
-                    </Typography>
 
-                    <FormContainer onSuccess={onSubmit}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12}>
-                                <TextFieldElement fullWidth name={"username"} type={"email"} required/>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <PasswordElement fullWidth name={"password"} required/>
-                            </Grid>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline/>
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+                    <LockOutlinedIcon/>
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Log in
+                </Typography>
+
+                <FormContainer onSuccess={onSubmit}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextFieldElement fullWidth name={"username"} type={"email"} required/>
                         </Grid>
-
-                        {error && (
-                            <Alert severity="error" sx={{marginTop: 2}}>{error}</Alert>
-                        )}
-
-                        <LoadingButton
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{mt: 3, mb: 2}}
-                            loading={loading}
-                        >
-                            Log In
-                        </LoadingButton>
-
-                    </FormContainer>
-
-                    <Grid container>
-                        <Grid item xs>
-                            <Link to="/signup">
-                                <Typography variant="body2">
-                                    Forgot password?
-                                </Typography>
-                            </Link>
-                        </Grid>
-                        <Grid item>
-                            <Link to="/signup">
-                                <Typography variant="body2">
-                                    Don't have an account? Sign Up
-                                </Typography>
-                            </Link>
+                        <Grid item xs={12}>
+                            <PasswordElement fullWidth name={"password"} required/>
                         </Grid>
                     </Grid>
 
-                </Box>
-                <CopyrightView sx={{mb: 4}}/>
-            </Container>
-        </ThemeProvider>
+                    {error && (
+                        <Alert severity="error" sx={{marginTop: 2}}>{error}</Alert>
+                    )}
+
+                    <LoadingButton
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{mt: 3, mb: 2}}
+                        loading={loading}
+                    >
+                        Log In
+                    </LoadingButton>
+
+                </FormContainer>
+
+                <Grid container>
+                    <Grid item xs>
+                        <Link to="/signup">
+                            <Typography variant="body2">
+                                Forgot password?
+                            </Typography>
+                        </Link>
+                    </Grid>
+                    <Grid item>
+                        <Link to="/signup">
+                            <Typography variant="body2">
+                                Don't have an account? Sign Up
+                            </Typography>
+                        </Link>
+                    </Grid>
+                </Grid>
+
+            </Box>
+            <CopyrightView sx={{mb: 4}}/>
+        </Container>
     );
 }
--- frontend/src/components/auth/SignUp.js
+++ frontend/src/components/auth/SignUp.js
@@ -6,7 +6,6 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import {createTheme, ThemeProvider} from '@mui/material/styles';
 import {useState} from "react";
 import LoadingButton from "@mui/lab/LoadingButton";
 import {Link, useNavigate, useParams} from "react-router-dom";
@@ -17,10 +16,6 @@ import {parseApiUrl, parseMessage} from "../../helpers/functions";
 import {useSignIn} from "react-auth-kit";
 import jwt_decode from "jwt-decode";
 import CopyrightView from "../common/CopyrightView";
-
-
-
-const theme = createTheme();
 
 export default function SignUp() {
     const [error, setError] = useState();
@@ -56,93 +51,90 @@ export default function SignUp() {
     }
 
     return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline/>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline/>
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+                    <LockOutlinedIcon/>
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
 
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
-                        <LockOutlinedIcon/>
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign up
-                    </Typography>
+                <FormContainer onSuccess={onSubmit}>
+                    <Grid container spacing={2}>
 
-                    <FormContainer onSuccess={onSubmit}>
-                        <Grid container spacing={2}>
-
-                            <Grid item xs={6}>
-                                <TextFieldElement fullWidth name={"firstName"} label={"First Name"} required/>
-                            </Grid>
-
-                            <Grid item xs={6}>
-                                <TextFieldElement fullWidth name={"lastName"} label={"Last Name"} required/>
-                            </Grid>
-
-                            <Grid item xs={12}>
-                                <TextFieldElement fullWidth name={"email"} label={"Email"} type={"email"} required/>
-                            </Grid>
-
-                            <Grid item xs={12}>
-                                <SelectElement
-                                    fullWidth
-                                    name={"accountType"}
-                                    label={"Register As"}
-                                    required
-                                    options={[
-                                        {id: 'student', title: 'Student'},
-                                        {id: 'lecturer', title: 'Lecturer'},
-                                    ]}/>
-                            </Grid>
-
-                            <Grid item xs={12}>
-                                <PasswordElement fullWidth name={"password"} label={"Password"} required/>
-                            </Grid>
-
-                            <Grid item xs={12}>
-                                <PasswordElement fullWidth name={"passwordConfirm"}
-                                                 label={"Password Confirmation"}
-                                                 type={"password"}
-                                                 required/>
-                            </Grid>
-
+                        <Grid item xs={6}>
+                            <TextFieldElement fullWidth name={"firstName"} label={"First Name"} required/>
                         </Grid>
 
-                        {error && (
-                            <Alert severity="error" sx={{marginTop: 2}}>{error}</Alert>
-                        )}
-
-                        <LoadingButton
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{mt: 3, mb: 2}}
-                            loading={loading}
-                        >
-                            Create Account
-                        </LoadingButton>
-
-                    </FormContainer>
-
-                    <Grid container justifyContent="flex-end">
-                        <Grid item>
-                            <Link to="/login">
-                                <Typography variant="body2">Already have an account? Sign in</Typography>
-                            </Link>
-
+                        <Grid item xs={6}>
+                            <TextFieldElement fullWidth name={"lastName"} label={"Last Name"} required/>
                         </Grid>
+
+                        <Grid item xs={12}>
+                            <TextFieldElement fullWidth name={"email"} label={"Email"} type={"email"} required/>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <SelectElement
+                                fullWidth
+                                name={"accountType"}
+                                label={"Register As"}
+                                required
+                                options={[
+                                    {id: 'student', title: 'Student'},
+                                    {id: 'lecturer', title: 'Lecturer'},
+                                ]}/>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <PasswordElement fullWidth name={"password"} label={"Password"} required/>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <PasswordElement fullWidth name={"passwordConfirm"}
+                                             label={"Password Confirmation"}
+                                             type={"password"}
+                                             required/>
+                        </Grid>
+
                     </Grid>
 
-                </Box>
-                <CopyrightView sx={{mt: 5}}/>
-            </Container>
-        </ThemeProvider>
+                    {error && (
+                        <Alert severity="error" sx={{marginTop: 2}}>{error}</Alert>
+                    )}
+
+                    <LoadingButton
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{mt: 3, mb: 2}}
+                        loading={loading}
+                    >
+                        Create Account
+                    </LoadingButton>
+
+                </FormContainer>
+
+                <Grid container justifyContent="flex-end">
+                    <Grid item>
+                        <Link to="/login">
+                            <Typography variant="body2">Already have an account? Sign in</Typography>
+                        </Link>
+
+                    </Grid>
+                </Grid>
+
+            </Box>
+            <CopyrightView sx={{mt: 5}}/>
+        </Container>
     );
 }
