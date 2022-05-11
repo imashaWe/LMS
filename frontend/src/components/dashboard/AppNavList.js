ad0c163f5b07b2a1661fb3aebf913479bc122f73
imasshaWe <imasha98.we@gmail.com>
2022-05-11 13:31:51 +0530

    refactor code

--- frontend/src/components/dashboard/AppNavList.js
+++ frontend/src/components/dashboard/AppNavList.js
@@ -1,32 +1,12 @@
 import * as React from 'react';
-import {ListItemButton, ListItemIcon, ListItemText, Divider, List} from "@mui/material";
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import GroupRoundedIcon from '@mui/icons-material/GroupRounded';
-import {useNavigate} from "react-router-dom";
+import {useAuthUser} from "react-auth-kit";
+import LecturerNavigation from "./navigations/LecturerNavigation";
+import StudentNavigation from "./navigations/StudentNavigation";
 
 const AppNavList = () => {
-    const navigate = useNavigate();
-    return (
-        <React.Fragment>
-            <List>
-                <ListItemButton onClick={() => navigate("/")}>
-                    <ListItemIcon>
-                        <DashboardIcon/>
-                    </ListItemIcon>
-                    <ListItemText primary="Dashboard"/>
-                </ListItemButton>
-            </List>
-            <Divider sx={{my: 1}}/>
-            <List>
-                <ListItemButton onClick={() => navigate("/blank")}>
-                    <ListItemIcon>
-                        <GroupRoundedIcon/>
-                    </ListItemIcon>
-                    <ListItemText primary="Users"/>
-                </ListItemButton>
-            </List>
-        </React.Fragment>
-    );
+    const auth = useAuthUser();
+    const isLecturer = auth().authorities.findIndex((a) => a.authority == "ROLE_LECTURER") != -1;
+    return isLecturer ? <LecturerNavigation/> : <StudentNavigation/>
 }
 
 export default AppNavList;
