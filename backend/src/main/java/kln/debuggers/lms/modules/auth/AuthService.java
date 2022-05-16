c637ddee15604f49ec9d5cb0192cb1445afae6ec
imasshaWe <imasha98.we@gmail.com>
2022-05-16 19:53:21 +0530

    add get auth user

--- backend/src/main/java/kln/debuggers/lms/modules/auth/AuthService.java
+++ backend/src/main/java/kln/debuggers/lms/modules/auth/AuthService.java
@@ -13,6 +13,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -38,7 +39,6 @@ public class AuthService {
         this.jwtTokenProvider = jwtTokenProvider;
         this.userDetailsService = userDetailsService;
     }
-
 
     AuthResponse login(User user) throws ResponseStatusException {
         try {
@@ -69,7 +69,6 @@ public class AuthService {
 
     }
 
-
     AuthResponse signup(Lecturer lecturer) throws ResponseStatusException {
         lecturer.setPassword(passwordEncoder.encode(lecturer.getPassword()));
         lecturer.setUsername(lecturer.getEmail());
@@ -84,5 +83,9 @@ public class AuthService {
 
     }
 
+    User getAuthUser() {
+        final Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return userRepository.findUserByUsername(auth.getName()).get();
+    }
 
 }
