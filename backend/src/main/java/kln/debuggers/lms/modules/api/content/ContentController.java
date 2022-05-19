27915cc854812ea2d86f450950eece823f5fc6ef
29Niro <59716887+29Niro@users.noreply.github.com>
2022-05-19 17:02:07 +0530

    Integrate lecture content view module (#9)
    * first commit
    
    * created lecturer course interface
    
    * implemented navigation to dashboard
    
    * implemented content view for lecturer
    
    * fix error in add new button
    
    * create content view for lecture
    
    * backend for content view
    
    * fix error in date input
    
    * replace radio button fix routes
    
    * create adding date column
    
    * fix error in delete button
    
    * remove an unused variable
    
    * date view modified
    
    * fix error in file input
    
    * change folder
    implement delete message
    
    * update routes & AppMessage
    
    * use preautherize method
    
    * update routes
    
    Co-authored-by: NethminiDevyanjalee <73274982+NethminiDevyanjalee@users.noreply.github.com>
    Co-authored-by: 29Niro <29iro@gmail.com>

---
+++ backend/src/main/java/kln/debuggers/lms/modules/api/content/ContentController.java
@@ -0,0 +1,40 @@
+package kln.debuggers.lms.modules.api.content;
+
+import kln.debuggers.lms.modules.storage.CloudStorageException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "content")
+@CrossOrigin
+public class ContentController {
+
+    @Autowired
+    private ContentService contentService;
+
+    @PostMapping("create/{courseID}")
+    @PreAuthorize("hasRole('ROLE_LECTURER')")
+    public ResponseEntity create(@ModelAttribute Content content, @PathVariable Long courseID) {
+        try {
+            contentService.addNewContent(content, courseID);
+        } catch (CloudStorageException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+        return ResponseEntity.ok("Successfully Saved");
+    }
+
+    @DeleteMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_LECTURER')")
+    public ResponseEntity delete(@PathVariable Long id) {
+        contentService.delete(id);
+        return ResponseEntity.ok("Successfully Deleted");
+    }
+
+    @GetMapping("{courseID}")
+    public ResponseEntity get(@PathVariable Long courseID) {
+        return ResponseEntity.ok(contentService.getContentsByCourse(courseID));
+    }
+}
