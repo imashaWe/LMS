f76aec3af4c0655d93fc9125369b63beaa920aa2
imasshaWe <imasha98.we@gmail.com>
2022-05-20 12:26:02 +0530

    add navigation uis

---
+++ mobile_app/lib/screens/app_navigation.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mylms/screens/account/account.dart';
+import 'package:mylms/screens/home/home.dart';
+import 'package:mylms/screens/my_courses/my_courses.dart';
+
+class AppNavigation extends StatefulWidget {
+  const AppNavigation({Key? key}) : super(key: key);
+
+  @override
+  State<AppNavigation> createState() => _AppNavigationState();
+}
+
+class _AppNavigationState extends State<AppNavigation> {
+  int _selectedIndex = 0;
+  void _change(int i) => setState(() => _selectedIndex = i);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(index: _selectedIndex, children: const [
+        Home(),
+        MyCourses(),
+        Account(),
+      ]),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.graduationCap), label: "My COurses"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Account"),
+        ],
+        onTap: _change,
+      ),
+    );
+  }
+}
---
+++ mobile_app/lib/screens/my_courses/my_courses.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyCourses extends StatelessWidget {
+  const MyCourses({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("My Courses")),
+    );
+  }
+}
