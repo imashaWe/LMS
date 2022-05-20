9e11dce8e64774af8237b9919950de124898cfb9
imasshaWe <imasha98.we@gmail.com>
2022-05-20 22:36:35 +0530

    update course model

--- mobile_app/lib/modules/course.dart
+++ mobile_app/lib/modules/course.dart
@@ -1,24 +1,31 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:mylms/modules/level.dart';
+import 'package:mylms/modules/subject.dart';
 import 'package:mylms/modules/user.dart';
 
 part 'course.g.dart';
 
 @JsonSerializable()
 class Course {
-  int? id;
-  String? title;
-  String? description;
+  int id;
+  String title;
+  String description;
   String? thumbnailURL;
-  int? duration;
-  User? lecturer;
+  int duration;
+  User lecturer;
+  Level level;
+  Subject subject;
 
-  Course(
-      {this.id,
-      this.title,
-      this.description,
-      this.duration,
-      this.thumbnailURL,
-      this.lecturer});
+  Course({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.duration,
+    required this.lecturer,
+    required this.level,
+    required this.subject,
+    this.thumbnailURL,
+  });
 
   factory Course.fromJson(Map<String, dynamic> json) => _$CourseFromJson(json);
   Map<String, dynamic> toJson() => _$CourseToJson(this);
--- mobile_app/lib/modules/course.g.dart
+++ mobile_app/lib/modules/course.g.dart
@@ -7,14 +7,14 @@ part of 'course.dart';
 // **************************************************************************
 
 Course _$CourseFromJson(Map<String, dynamic> json) => Course(
-      id: json['id'] as int?,
-      title: json['title'] as String?,
-      description: json['description'] as String?,
-      duration: json['duration'] as int?,
+      id: json['id'] as int,
+      title: json['title'] as String,
+      description: json['description'] as String,
+      duration: json['duration'] as int,
+      lecturer: User.fromJson(json['lecturer'] as Map<String, dynamic>),
+      level: Level.fromJson(json['level'] as Map<String, dynamic>),
+      subject: Subject.fromJson(json['subject'] as Map<String, dynamic>),
       thumbnailURL: json['thumbnailURL'] as String?,
-      lecturer: json['lecturer'] == null
-          ? null
-          : User.fromJson(json['lecturer'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$CourseToJson(Course instance) => <String, dynamic>{
@@ -24,4 +24,6 @@ Map<String, dynamic> _$CourseToJson(Course instance) => <String, dynamic>{
       'thumbnailURL': instance.thumbnailURL,
       'duration': instance.duration,
       'lecturer': instance.lecturer,
+      'level': instance.level,
+      'subject': instance.subject,
     };
