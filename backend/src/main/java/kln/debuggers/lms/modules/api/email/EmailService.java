7f52bb2432bb3864b1740c75692d3e9c82b56773
imasshaWe <imasha98.we@gmail.com>
2022-05-21 22:32:23 +0530

    add emails templates

--- backend/src/main/java/kln/debuggers/lms/modules/api/email/EmailService.java
+++ backend/src/main/java/kln/debuggers/lms/modules/api/email/EmailService.java
@@ -1,5 +1,6 @@
 package kln.debuggers.lms.modules.api.email;
 
+import kln.debuggers.lms.modules.api.auth.student.Student;
 import kln.debuggers.lms.modules.api.content.Content;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -14,7 +15,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 public class EmailService {
@@ -24,29 +24,42 @@ public class EmailService {
     private SpringTemplateEngine thymeleafTemplateEngine;
 
     public void sendContentMail(Content content) throws MessagingException {
-        Context thymeleafContext = new Context();
         Map<String, Object> data = new HashMap<>();
-        data.put("title", content.getCourse().getTitle());
-        thymeleafContext.setVariables(data);
-        String body = thymeleafTemplateEngine.process("content-email.html", thymeleafContext);
-        List<String> emails = content.getCourse()
-                .getStudentList()
-                .stream()
-                .map(e -> e.getEmail()).collect(Collectors.toList());
 
-        send(content.getName(), body, emails);
+        data.put("course", content.getCourse().getTitle());
+        data.put("description", content.getDescription());
+        data.put("title", content.getName());
+        data.put("date", content.getDueDate());
+        data.put("url", content.getFileURL());
 
-    }
+        List<Student> studentList = content.getCourse()
+                .getStudentList();
 
-    private void send(String subject, String body, List<String> emails) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name());
-        helper.setSubject(subject);
-        helper.setText(body, true);
-        for (String email : emails) {
-            helper.setTo(email);
+        helper.setSubject(content.getName());
+        String template;
+
+        if (content.getType().equals("Announcement")) {
+            template = "announcement-email.html";
+        } else {
+            template = "content-email.html";
+        }
+
+        for (Student student : studentList) {
+            data.put("student", student.getFirstName() + " " + student.getLastName());
+            helper.setText(getTemplate(data, template), true);
+            helper.setTo(student.getEmail());
             mailSender.send(message);
         }
 
     }
+
+    private String getTemplate(Map<String, Object> data, String template) {
+        Context thymeleafContext = new Context();
+        thymeleafContext.setVariables(data);
+        return thymeleafTemplateEngine.process(template, thymeleafContext);
+    }
+
+
 }
