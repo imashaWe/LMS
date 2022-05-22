3cc8956a6805193cfaa36b9750463577d7fad2f6
Asitha Nuwan <61353438+savitarasitha@users.noreply.github.com>
2022-05-22 12:25:36 +0530

    Integrate submission (#21)
    * integrate submission
    
    * integrate submission controller
    
    * integrate submission mark
    
    * integrate submission repository
    
    * integrate submission service
    
    * add lecturer submissons
    
    * add studented submissions views
    
    * update routes
    
    * add dependecies
    
    * update api

---
+++ backend/src/main/java/kln/debuggers/lms/modules/api/submission/SubmissionMark.java
@@ -0,0 +1,22 @@
+package kln.debuggers.lms.modules.api.submission;
+
+public class SubmissionMark {
+    private int marks;
+    private String comment;
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
